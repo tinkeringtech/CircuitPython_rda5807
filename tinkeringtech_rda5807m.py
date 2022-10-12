f2f2e34978fc4ade0c52a73dd46567bf61f8c031
Tinkeringtech <1667122+tinkeringtech@users.noreply.github.com>
2022-10-11 21:32:16 -0400

    Update tinkeringtech_rda5807m.py
    updates to implementation notes.

--- tinkeringtech_rda5807m.py
+++ tinkeringtech_rda5807m.py
@@ -16,16 +16,12 @@ Implementation Notes
 
 **Hardware:**
 
-.. todo:: Add links to any specific hardware product page(s), or category page(s).
-  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+* A FM radio board based on the RDA5807m module.
 
 **Software and Dependencies:**
 
 * Adafruit CircuitPython firmware for the supported boards:
   https://circuitpython.org/downloads
-
-.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
-  based on the library's use of either.
 
 # * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
 # * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
